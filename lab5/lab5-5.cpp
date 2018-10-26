04c76d22d0fc69c506e31153721f122b9949010f
Luzzy <luzzyzerio@gmail.com>
2018-10-26 19:42:37 +0800

    lab5 finished

--- lab5/lab5-5.cpp
+++ lab5/lab5-5.cpp
@@ -1,52 +1,52 @@
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <cstring>
 
 using namespace std;
-int test,n,m;
-string S,T;
+int test, n, m;
+string S, T;
 int nextList[100000];
-int matches[100000];
-void mknext(){
+void mknext()
+{
     // memset(nextList,0,100000);
-    int i,j;
-    nextList[0]=0;
-    for(i=1, j=0;i<n-1;i++){
-        while(j>0&&S[i]!=S[j])
-            j=nextList[j-1];
-        if(S[i]==S[j]){
+    int i, j;
+    nextList[0] = -1;
+    for (int i = 0, j = -1; i < n;)
+    {
+        if (j == -1 || S[i] == S[j])
+            nextList[++i] = ++j;
+        else
+            j = nextList[j];
+    }
+}
+int match()
+{
+    int j = 0;
+    for (int i = 0; i < m;)
+    {
+        if (j == -1 || T[i] == S[j])
+        {
+            i++;
             j++;
         }
-        nextList[i]=j;
+        else
+            j = nextList[j];
+
     }
-    
+        return j;
 }
-int match(){
-    matches[0]=0;
-    for(int i=1,j=0;i<m;i++){
-        while(j>0&&T[i]!=S[j])
-            j=nextList[j-1];
-        if(T[i]==S[j])
-            j++;
-        matches[i]=j;
-        // cout << j<<" ";
-        if(j==n-1)
-            j=nextList[j-1];
-        
-    }
-    return matches[m-1];
-}
-int main(){
+int main()
+{
     cin >> test;
-    while(test--){
-        cin >>n>>m>>S>>T;
+    while (test--)
+    {
+        cin >> n >> m >> S >> T;
         mknext();
-        int num =match();
-        // cout<<num;
-        string s=S.substr(0,num);
-        printf("%d %s\n",num,s.c_str());
-        // if(num!=0){
-        //     cout<<" "<<S.substr(0,num);
-        // }
-        // cout <<endl;
+        int num = match();
+        cout << num;
+        if (num)
+        {
+            cout << " " << S.substr(0, num);
+        }
+        cout << endl;
     }
 }
